a944174d37d8795cc0ac55bf54de5ca2dcb7d56b
pgaillot <p.gaillot@essteam.fr>
2023-02-25 17:06:18 +0100

    update pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTabsModule} from '@angular/material/tabs';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { SkillsComponent } from './pages/skills/skills.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    AboutMeComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
