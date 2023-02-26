b1baa0132075a2c7109ae0e2d40e0f6a8890d613
pgaillot <p.gaillot@essteam.fr>
2023-02-26 10:11:18 +0100

    add projects.json

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Project } from 'src/app/models/project';
 
@@ -7,34 +8,17 @@ import { Project } from 'src/app/models/project';
   styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent {
-  projects: Project[] = [
-    {
-      displayName: 'test',
-      description:
-        'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.',
-      imgUrl:
-        'https://this-person-does-not-exist.com/img/avatar-11ab00b25f22ad77edee40e6c9bdfdb3.jpg',
-    },
-    {
-      displayName: '2',
-      description:
-        'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.',
-      imgUrl:
-        'https://this-person-does-not-exist.com/img/avatar-11ab00b25f22ad77edee40e6c9bdfdb3.jpg',
-    },
-    {
-      displayName: '3',
-      description:
-        'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.',
-      imgUrl:
-        'https://this-person-does-not-exist.com/img/avatar-11ab00b25f22ad77edee40e6c9bdfdb3.jpg',
-    },
-    {
-      displayName: '4',
-      description:
-        'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.',
-      imgUrl:
-        'https://this-person-does-not-exist.com/img/avatar-11ab00b25f22ad77edee40e6c9bdfdb3.jpg',
-    },
-  ];
+  projects: any = [];
+
+  constructor(
+    private httpClient: HttpClient
+    ){}
+    
+  ngOnInit(){
+    this.httpClient.get("assets/projects.json").subscribe(data =>{
+      console.log(data);
+      const projectsData = data;
+      this.projects = projectsData;
+    })
+  }
 }
