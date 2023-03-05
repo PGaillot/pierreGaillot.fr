d46e2027fad3afb41b044f71db8a38dfee93f1f2
pgaillot <p.gaillot@essteam.fr>
2023-03-05 11:57:53 +0100

    update projects

--- src/app/components/skill-item/skill-item.component.ts
+++ src/app/components/skill-item/skill-item.component.ts
@@ -8,5 +8,21 @@ import { Skill } from 'src/app/models/skill';
 })
 export class SkillItemComponent {
   @Input() skill!:Skill;
+  skillColorAlpha:string = "";
+  
+  ngOnInit(): void {
+
+    this.setSkillColorAlpha();
+  }
+
+
+  setSkillColorAlpha() {
+    const opacity:string = "15";
+    if(this.skill.color != null){
+      this.skillColorAlpha = this.skill.color + opacity;
+    } else {
+      this.skillColorAlpha = "#cccccc" + opacity;
+    }
+  }
 
 }
