20e1b62bba6f033fe47b2457bc1f2da63e628b05
pgaillot <p.gaillot@essteam.fr>
2023-03-14 11:47:06 +0100

    delete import useless

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Project } from '../models/project';
-import { Skill } from '../models/skill';
 
 @Injectable({
   providedIn: 'root',
